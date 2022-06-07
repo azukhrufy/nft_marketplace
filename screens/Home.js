b616880bd197422452dc1ff91a0e9a3d61b1609c
azukhrufy <anandazukhrufawalwi@gmail.com>
2022-06-07 15:56:36 +0700

    initial project

---
+++ screens/Home.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { View, SafeArea, FlatList,Text } from 'react-native';
+
+const Home = () => {
+    return (
+        <Text>
+            Hooooome
+        </Text>
+    )
+}
+
+export default Home
