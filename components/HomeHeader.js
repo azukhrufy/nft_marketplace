2f46d419fa8fbc55995fd6be7a4f793f61b5880e
azukhrufy <anandazukhrufawalwi@gmail.com>
2022-06-08 17:10:39 +0700

    feat(NFTCard) : implement NFTCard with basic Img and Like Button

---
+++ components/HomeHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const HomeHeader = () => {
+    return (
+        <View>
+            <Text>Home Header</Text>
+        </View>
+    )
+}
+
+export default HomeHeader
---
+++ components/SubInfo.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export const NFTTitle = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export const ETHPrice = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export const ImageCmp = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export const People = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export const EndDate = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export const SubInfo = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
